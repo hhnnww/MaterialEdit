ab90aa85aebea717bc08a341984b217465400b86
weihua <175990271@qq.com>
2022-10-28 17:45:04 +0800

    1

---
+++ module_素材采集/core/class_envato.py
@@ -0,0 +1,64 @@
+import re
+from module_素材采集.core.class_htmldown import HTMLDown
+from module_素材采集.core.model import MaterialType
+import json
+from hashlib import sha256
+
+
+class SCEnvato:
+    def __init__(self, start_url: str, max_page: int, cookie: str = ''):
+        if 'pg-' not in start_url.split('/')[-1]:
+            start_url = start_url + '/pg-1'
+
+        self.start_url = start_url
+        self.max_page = max_page
+        self.cookie = cookie
+
+    def fun_列表页(self):
+        for x in range(1, self.max_page + 1):
+            url = re.sub('/pg-\d+', f'/pg-{x}', self.start_url)
+            yield url
+
+    def fun_列表页中获取素材(self, url):
+        html = HTMLDown(url, cookie=self.cookie).html
+
+        script_text = ''
+
+        for in_script in html.find('script'):
+            if 'window.INITIAL_HYDRATION_DATA' in in_script.text:
+                script_text = in_script.text
+
+        script_text = re.findall('(\{[\s\S]*?});', script_text)[0]
+
+        items: dict = json.loads(script_text)
+        items = items.get('page').get('data').get('items')
+
+        for item in items:
+            item: dict
+            url = f'https://elements.envato.com/item-{item.get("id")}'
+            img = ''
+
+            for img_size in ['w632', 'w710', 'w866', 'w900']:
+                in_img = item.get('coverImage').get(img_size)
+                if in_img is not None:
+                    img = in_img
+
+            yield MaterialType(
+                url=url,
+                img=img,
+                hash=sha256(url.encode('utf-8')).hexdigest()
+            )
+
+    def main(self):
+        for page in self.fun_列表页():
+            for ma in self.fun_列表页中获取素材(page):
+                yield ma
+
+
+if __name__ == '__main__':
+    sce = SCEnvato(
+        start_url=r'https://elements.envato.com/graphic-templates/product-mockups/sort-by-latest/pg-2',
+        max_page=20
+    ).main()
+    for ma_obj in sce:
+        print(ma_obj)
